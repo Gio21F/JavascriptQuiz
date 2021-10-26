842b833e6244519c9777bf992882338dd17c7594
Giovani <giovanigonzalezchavez@gmail.com>
2021-10-26 18:13:03 -0500

    Quiz Javascript

---
+++ data/data.js
@@ -0,0 +1,17 @@
+export const data = [
+    {
+        question: "Which of the following is not a code editor? ",
+        choices: ["vim", "vscode", "emacs", "word"],
+        answer: "word",
+    },
+    {
+        question: "What language is not object-oriented?",
+        choices: ["Java", "Haskell", "C++", "Python"],
+        answer: "Haskell",
+    },
+    {
+        question: "What language is not good for styling websites?",
+        choices: ["Stylus", "Less", "Sass", "CSS", 'PostCSS', 'NextCSS'],
+        answer: "NextCSS",
+    },
+]
